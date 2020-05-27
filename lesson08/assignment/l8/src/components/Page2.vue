db47ad323a78a553b286f7e3eaab364b9d4f41ad
Taylor Sommers <taylorsommers@Taylors-MacBook-Pro.local>
2020-05-26 21:03:55 -0700

    create 08 project with files from 07

---
+++ lesson08/assignment/l8/src/components/Page2.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <div class="card-group p-5" v-for="(cardinfo, index) in cardinfos" :key="index">
+          <img :src="cardinfo.image" class="img-fluid m-5" style="width: 18rem;">
+          <!-- <img :src="cardinfo.image" class="img-fluid m-5" style="width: 18rem;">
+          <img :src="cardinfo.image" class="img-fluid m-5" style="width: 18rem;"> -->
+        </div>
+
+      <form>
+        <div class="form-group m-1 p-1" v-for="(forminfo, index) in forminfos">
+          <label for="forminfo.fname">{{forminfo.fname}}</label>
+          <input class="form-control" type="text" id="name" :placeholder="forminfo.placeholder">
+        </div>
+        <button type="button" class="btn btn-lg btn-danger m-2" data-toggle="popover" title="Popover title" data-content="Before you click submit, make sure you have double checked all of your info.">Submit</button>
+
+      </form>
+  </div>
+</template>
+
+<script>
+
+  var cards = [{
+
+      image: require('../assets/food3.jpg'),
+  },
+  {
+
+      image: require('../assets/food4.jpg'),
+  },
+  {
+
+      image: require('../assets/food5.jpg'),
+  }]
+
+  var form = [
+    {
+        fname: "Name",
+        placeholder: "Enter Name"
+    },
+    {
+        fname: "Address",
+        placeholder: "Enter Address"
+    },
+    {
+        fname: "Phone Number",
+        placeholder: "Enter Phone Number"
+    },
+    {
+        fname: "Email",
+        placeholder: "Enter Email"
+    }
+]
+
+  export default {
+    name: 'Page2',
+    data() {
+      return {
+        cardinfos: cards,
+        currentIdx: 0,
+        forminfos: form
+      }
+    }
+  }
+
+
+</script>
---
+++ lesson08/assignment/l8/src/components/Page3.vue
@@ -0,0 +1,17 @@
+<!-- <template>
+  <div>
+    <div class="row">
+       <div class="col-xl-2 col-lg-8 col-sm-2 col-12 p-5" >
+         <img src="images/nature8.jpg" class="img-fluid m-1">
+         <img src="images/nature8.jpg" class="img-fluid m-1">
+         <img src="images/nature8.jpg" class="img-fluid" m-1>
+
+       </div>
+
+       <div class="col-xl-10 col-lg-4 col-sm-10 col-12 p-5">
+         <img src="images/nature9.jpg" class="img-fluid">
+
+       </div>
+     </div>
+  </div>
+</template> -->
